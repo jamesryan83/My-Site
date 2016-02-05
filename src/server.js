bfe796c3f220b41cf3b5fc7d8149345679b7f77b
James <james4165@hotmail.com>
2016-02-06 09:44:17 +1000

    Update

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-var fs = require("fs");
 var express = require("express");
 var path = require("path");
 
@@ -13,28 +12,10 @@ app.get("/", function (req, res) {
 });
 
 
-// Get my resume in pdf format
-app.get("/get-james-resume", function (req, res) {
-
-    var pdfFile = path.join(__dirname, "public", "res", "James Ryan Resume.pdf");
-
-    fs.readFile(pdfFile, function (err, data){
-        if (err) {
-            console.log(err);
-            res.end();
-        }
-
-        res.setHeader("content-type", "text/pdf");
-        res.end(data);
-    });
-});
-
-
 // redirect everything to home page
 app.all("*", function (req, res) {
     res.redirect("/");
 });
-
 
 
 // Start Server
